e288bf81937b5a06266442605d4d5637429a9a01
huness <tialdorf@gmail.com>
2018-12-01 19:31:11 +0300

    +productsSlider section as mixin, 2 remnants-inner slider blocks

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -3220,6 +3220,77 @@ $(window).scroll(function () {
 "use strict";
 "use strict";
 
+var $productsRemnantsSlider = $(".remnantsPageSlider");
+
+$productsRemnantsSlider.slick({
+    slidesToScroll: 1,
+    slidesToShow: 4,
+    focusOnSelect: false,
+    prevArrow: ".remnantsPageSlider__btn-prev",
+    nextArrow: ".remnantsPageSlider__btn-next",
+    responsive: [{
+        breakpoint: 1280,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+    }, {
+        breakpoint: 960,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+    }, {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    }, {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    }]
+});
+
+var $productsRemnantsReleatedSlider = $(".remnantsReleatedPageSlider");
+
+$productsRemnantsReleatedSlider.slick({
+    slidesToScroll: 1,
+    slidesToShow: 4,
+    focusOnSelect: false,
+    prevArrow: ".remnantsReleatedPageSlider__btn-prev",
+    nextArrow: ".remnantsReleatedPageSlider__btn-next",
+    responsive: [{
+        breakpoint: 1280,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+    }, {
+        breakpoint: 960,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+    }, {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    }, {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    }]
+});
+"use strict";
+
 var $projectsSlider = $(".projectsSlider__inner");
 
 $projectsSlider.slick({
---
+++ markup/components/productsSliderSection/productsSliderSection.js
@@ -0,0 +1,79 @@
+var $productsRemnantsSlider = $(".remnantsPageSlider");
+
+$productsRemnantsSlider.slick({
+    slidesToScroll: 1,
+    slidesToShow: 4,
+    focusOnSelect: false,
+    prevArrow: ".remnantsPageSlider__btn-prev",
+    nextArrow: ".remnantsPageSlider__btn-next",
+    responsive: [
+        {
+            breakpoint: 1280,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 960,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+var $productsRemnantsReleatedSlider = $(".remnantsReleatedPageSlider");
+
+$productsRemnantsReleatedSlider.slick({
+    slidesToScroll: 1,
+    slidesToShow: 4,
+    focusOnSelect: false,
+    prevArrow: ".remnantsReleatedPageSlider__btn-prev",
+    nextArrow: ".remnantsReleatedPageSlider__btn-next",
+    responsive: [
+        {
+            breakpoint: 1280,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 960,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
